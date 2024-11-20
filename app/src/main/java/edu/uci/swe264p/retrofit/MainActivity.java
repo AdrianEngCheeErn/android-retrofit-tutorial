385b26ffc101c37134d110c9334d6f557cf42909
Adrian Eng Chee Ern <adrianengcheeern@gmail.com>
2024-11-20 12:07:15 +0800

    test
    test

--- app/src/main/java/edu/uci/swe264p/retrofit/MainActivity.java
+++ app/src/main/java/edu/uci/swe264p/retrofit/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     static final String TAG = MainActivity.class.getSimpleName();
     static final String BASE_URL = "https://api.themoviedb.org/3/";
     static Retrofit retrofit = null;
-    final static String API_KEY = "YOUR_API_KEY";
+    final static String API_KEY = "0727ecab6bc0e791a8c8b5959a6ce5b4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
